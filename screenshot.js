8aec87fe6ffc8617317f0f32c9270b6ec01d4596
Owen Williams <owilliams@mixxx.org>
2022-08-15 16:40:00 -0400

    Don't hard-code home dir

--- screenshot.js
+++ screenshot.js
@@ -2,24 +2,16 @@ const puppeteer = require('puppeteer-core');
 
 (async () => {
 
-  // 1. Launch the browser
-  //  const browser = await puppeteer.launch();
+  const homedir = require('os').homedir();
   const browser = await puppeteer.launch({
-        executablePath: '/usr/bin/chromium-browser', 
-        userDataDir: '/home/soto/.config/chromium',
+        executablePath: '/usr/bin/chromium-browser',
+        userDataDir: homedir + '/.config/chromium',
         defaultViewport: {width: 600, height: 448}
   });
 
-  // 2. Open a new page
   const page = await browser.newPage();
-
-  // 3. Navigate to URL
   await page.goto('https://soto.grafana.net/public-dashboards/d7a1f14e586a48ca8b072cb5694910ae?theme=light');
-
   await page.waitForNavigation({waitUntil: 'networkidle2'});
-
-        // 4. Take screenshot
   await page.screenshot({path: 'screenshot.png'});
-
   await browser.close();
 })();
