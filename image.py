81f50323e9ad330bb5bfe6b907ec4613b26212c5
Owen Williams <owilliams@mixxx.org>
2022-08-15 17:06:54 -0400

    Add command line arguments

--- image.py
+++ image.py
@@ -16,10 +16,10 @@ Usage: {file} image-file
     sys.exit(1)
 
 image = Image.open(sys.argv[1])
-#resizedimage = image.resize(inky.resolution)
+resizedimage = image.resize(inky.resolution)
 
 if len(sys.argv) > 2:
     saturation = float(sys.argv[2])
 
-inky.set_image(image, saturation=saturation)
+inky.set_image(resizedimage, saturation=saturation)
 inky.show()
